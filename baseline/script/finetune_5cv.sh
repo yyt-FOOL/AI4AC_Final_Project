502a6b8b574cc43de363683c89aae4233b2a30a5
yyt <37420222204267@xmu.edu.cn>
2025-06-08 10:07:20 +0800

    move data to outer folder

--- baseline/script/finetune_5cv.sh
+++ baseline/script/finetune_5cv.sh
@@ -2,7 +2,7 @@ data_path="../data/finetune" # replace to your data path
 n_gpu=1  
 MASTER_PORT=33332
 num_classes=1
-weight_path='../../../weights'  # replace to your pre-training ckpt path
+weight_path='../../../../weights'  # replace to your pre-training ckpt path
 weight_name='pretraining_molecular'
 dict_name='dict'
 lr=0.0001 
@@ -25,7 +25,7 @@ atom_des=0
 
 global_batch_size=`expr $batch_size \* $n_gpu \* $update_freq`
 ### modified the save directory
-save_dir="../output/finetune/5cv/${selected_atom}_${weight_name}_global_${11}_kener_${12}_atomdes_${13}_${arch}_${loss}_lr_${lr}_bs_${global_batch_size}_${warmup}_${epoch}"
+save_dir="../../../output_data/baseline/output/finetune/5cv/${selected_atom}_${weight_name}_global_${11}_kener_${12}_atomdes_${13}_${arch}_${loss}_lr_${lr}_bs_${global_batch_size}_${warmup}_${epoch}"
 if [ -d "${save_dir}" ]; then
     rm -rf ${save_dir}
     echo "Folder remove at: ${save_dir}"
